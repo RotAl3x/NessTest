5822c4383221029a9237b3ea63a5b894a70012db
RotAl3x <alexandru.lolea@student.upt.ro>
2022-03-15 09:14:01 +0200

    1 file modified

--- src/app/components/classes-table/classes-table.component.ts
+++ src/app/components/classes-table/classes-table.component.ts
@@ -1,52 +1,49 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {Data} from "../../interfaces/data.interface";
-import {DataService} from "../../services/data.service";
-import {MatDialog} from "@angular/material/dialog";
-import {DialogComponent} from "../dialog/dialog.component";
-import {SearchComponent} from "../search/search.component";
-import {classes} from "../../data/data";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Data } from '../../interfaces/data.interface';
+import { DataService } from '../../services/data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from '../dialog/dialog.component';
+import { SearchComponent } from '../search/search.component';
+import { classes } from '../../data/data';
 
 @Component({
   selector: 'app-classes-table',
   templateUrl: './classes-table.component.html',
-  styleUrls: ['./classes-table.component.scss']
+  styleUrls: ['./classes-table.component.scss'],
 })
 export class ClassesTableComponent implements OnInit {
-
   @ViewChild(SearchComponent)
   private search!: SearchComponent;
   classes: Data[] = [];
-  initialClasses !: Data[];
+  initialClasses!: Data[];
   name!: string;
   searchName!: string;
 
-  constructor(private dataService: DataService,
-              public dialog: MatDialog) {
+  constructor(private dataService: DataService, public dialog: MatDialog) {
     this.classes = dataService.getClasses();
     this.initialClasses = classes;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   openDialog(data: Data) {
     const dialogRef = this.dialog.open(DialogComponent);
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) this.dataService.update(result, data)
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) this.dataService.update(result, data);
     });
     this.initialClasses = classes;
   }
 
-
   percentage(data: Data): number {
-    return Math.round(data.students.length / data.maxNoOfStudents * 100);
+    return Math.round((data.students.length / data.maxNoOfStudents) * 100);
   }
 
   deleteClass(data: Data) {
     try {
       this.dataService.deleteClass(data);
-      this.classes.splice(classes.indexOf(data),0);
+      if (this.initialClasses !== this.classes)
+        this.classes.splice(classes.indexOf(data), 1);
     } catch (e) {
       console.error(e);
     } finally {
@@ -55,13 +52,13 @@ export class ClassesTableComponent implements OnInit {
   }
 
   isFull(data: Data) {
-    return (data.maxNoOfStudents - data.students.length) > 0;
+    return data.maxNoOfStudents - data.students.length > 0;
   }
 
   filterClasses(searchTerm: string) {
     this.classes = this.initialClasses;
-    this.classes = this.classes.filter(x => eval('x'+'.'+searchTerm).indexOf(this.searchName) !== -1)
+    this.classes = this.classes.filter(
+      (x) => eval('x' + '.' + searchTerm).indexOf(this.searchName) !== -1
+    );
   }
-
-
 }
